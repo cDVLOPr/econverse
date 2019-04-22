2752f657e990c5db1b08ced7503cf4b17c691b57
Vinicius Alarcon <viniciusgarciaalarcon@hotmail.com>
2019-04-22 20:42:24 -0300

    include event click in the section images

--- js/product.js
+++ js/product.js
@@ -1,9 +1,8 @@
-// main image
-$(".e-product-image");
-
-// mini images
-$(".e-product-image1").click(function(event) {
-	// $(".e-product-image").css('border', '3px solid green');
-	$(".e-product-image").replaceWith( '<div class="e-product-image"> <figure> <a href="javascript:void()"><img src="images/03-image-mini-2_07.jpg" alt="Product image small"></a> </figure> </div>' );
+$(".e-product-image-small-container a").click(function(e) {
+	e.preventDefault();
+	var image_src = $(this).find('img').attr('src');
+	$('.e-product-image img').attr('src', image_src);
 });
-// .css({"border":"5px solid orange"});
+
+
+// slide toogle
