fbee1ceace8e5e9d4d38d6e47e69f03cdbd7b19b
Vinicius Alarcon <viniciusgarciaalarcon@hotmail.com>
2019-04-23 18:26:20 -0300

    created effect accordion in the section description

--- js/product.js
+++ js/product.js
@@ -1,8 +1,15 @@
+// replace images
 $(".e-product-image-small-container a").click(function(e) {
 	e.preventDefault();
 	var image_src = $(this).find('img').attr('src');
 	$('.e-product-image img').attr('src', image_src);
 });
 
-
 // slide toogle
+$( ".e-info-product button" ).on('click', function(e) {
+	// toggleClass = add and remove css class, siblings = brother elements, slideToggle = apply the effect
+	$(this).toggleClass('e-active').siblings().slideToggle("slow");
+
+	// access the father element and search the "P" element to put the slideToggle effect  *other way with the same result*
+	// $(this).parent().find('p').slideToggle("slow");
+});
